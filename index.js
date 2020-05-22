08cf4fc417c539c6a176e08656b64012a8e4ef9d
SpyrosGeo <xarodasimba@gmail.com>
2020-05-22 13:30:39 +0300

    added basic structure

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 // express alternative for deno
+import {PORT} from './config.js'
 
+import router from "./router.js"
+
+const app = new Application();
+app.use(router.routes());
+//  we must expilicly allow in deno 
+app.use(router.allowMethods())
+
+console.log(`server is running  at https://localhost:${PORT}`);
+
+await app.listen({port:PORT});
